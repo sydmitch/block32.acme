504145034303f953720f16335c5258f9acfb0dcb
Sydney Mitchell <samitchell04@gmail.com>
2025-03-12 18:48:31 -0500

    working on GET requests

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
-const pg = require('pg')
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(require('morgan')('dev'))
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/api/flavors', async (req, res, next) => {
   try {
@@ -13,20 +14,35 @@ app.get('/api/flavors', async (req, res, next) => {
     SELECT * FROM flavors
     `;
     const response = await client.query(SQL);
-    console.log('response is =', response.rows);
+    console.log('response is = ', response.rows);
     res.send(response.rows);
   } catch (error) {
     next(error);
   }
 });
 
-app.post('api/flavors', async (req, res, next) => {
+app.get('/api/flavors/:id', async (req, res, next) => {
+  try {
+    const SQL = /*sql*/`
+      SELECT * FROM flavors WHERE id = $1;
+    `;
+    const response = await client.query(SQL, [req.params.id]);
+    if (response.rows.length === 0) {
+      return res.status(404).send({ error: "Flavor not found" });
+    }
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/api/flavors', async (req, res, next) => {
   try {
     console.log('req.body => ', req.body);
     const SQL = /*sql*/ `
-      INSERT INTO flavors(txt) VALUES($1) RETURNING *;
+      INSERT INTO flavors(flavorName, is_favorite) VALUES($1, $2) RETURNING *;
     `
-    const response = await client.query(SQL, [req.body.txt]);
+    const response = await client.query(SQL, [req.body.flavorName, req.body.is_favorite]);
     console.log('response rows', response.rows);
     res.send(response.rows[0]);
   } catch (error) {
@@ -34,17 +50,17 @@ app.post('api/flavors', async (req, res, next) => {
   }
 });
 
-app.put('api/flavors', async (req, res, next) => {
-  try{
+app.put('/api/flavors/:id', async (req, res, next) => {
+  try {
     console.log('req.body => ', req.body);
     const SQL = /*sql*/ `
       UPDATE flavors
-      SET txt=$1, is_favorite=$2, updated_at=now()
+      SET flavorName=$1, is_favorite=$2, updated_at=now()
       WHERE id=$3
       RETURNING *
     `
     console.log('req.params => ', req.params);
-    const response = await client.query(SQL, [req.body.txt, req.body.is_favorite, req.params.id]);
+    const response = await client.query(SQL, [req.body.flavorName, req.body.is_favorite, req.params.id]);
     console.log('response => ', response.rows);
     res.send(response.rows[0]);
   } catch (error) {
@@ -52,25 +68,45 @@ app.put('api/flavors', async (req, res, next) => {
   }
 });
 
-const init = async () => {
+app.delete('/api/flavors/:id', async (req, res, next)=> {
+  try {
+    const SQL = /*sql*/`
+      DELETE from flavors
+      WHERE id = $1
+    `
+    const response = await client.query(SQL, [req.params.id])
+    res.sendStatus(204)
+  } catch (ex) {
+    next(ex)
+  }
+});
+
+
+
+async function init() {
+  console.log("🛢️ Connecting to database:", process.env.DATABASE_URL)
   await client.connect();
   console.log('connected to database');
-  let SQL = /*sql*/`
+  const SQL = /*sql*/`
     DROP TABLE IF EXISTS flavors;
     CREATE TABLE flavors(
       id SERIAL PRIMARY KEY,
       created_at TIMESTAMP DEFAULT now(),
       updated_at TIMESTAMP DEFAULT now(),
-      txt VARCHAR(255) NOT NULL,
+      flavorName VARCHAR(255) NOT NULL,
       is_favorite BOOLEAN DEFAULT FALSE
     );
-    INSERT INTO flavors(txt, is_favorite) VALUES('vanilla', true);
-    INSERT INTO flavors(txt, is_favorite) VALUES('chocolate', false);
+    INSERT INTO flavors(flavorName, is_favorite) VALUES('vanilla', TRUE);
+    INSERT INTO flavors(flavorName, is_favorite) VALUES('chocolate', FALSE);
+    INSERT INTO flavors(flavorName, is_favorite) VALUES('cookies and cream', FALSE);
+    INSERT INTO flavors(flavorName, is_favorite) VALUES('peanut butter cup', TRUE);
   `;
+  console.log("Creating table flavors...");
   await client.query(SQL);
-  console.log('data seeded');
+  console.log("Table created successfully.");
+  const PORT = process.env.PORT || 3000;
   app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 };
 
